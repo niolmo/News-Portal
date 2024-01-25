88b330697a037876645d1cb9d6d8b092597b0a34
niolmo <71871013+niolmo@users.noreply.github.com>
2024-01-25 13:35:22 +0500

    repag

--- NewPap/news/urls.py
+++ NewPap/news/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .views import Post_List
 
 app_name = 'news'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    # path('', views.post_list, name='post_list'),
+    path("", views.Post_List.as_view(), name="post_list"),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- NewPap/news/views.py
+++ NewPap/news/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404  # вернутьобъект или 404
 from django.core.paginator import Paginator
+from django.views.generic import ListView
 
 from .models import Post
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 6)
-    page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
-    return render(request, 'list.html', {'posts': posts})
+# def post_list(request):
+#     post_list = Post.published.all()
+#     paginator = Paginator(post_list, 6)
+#     page_number = request.GET.get('page', 1)
+#     posts = paginator.page(page_number)
+#     return render(request, 'list.html', {'posts': posts})
+
+
+class Post_List(ListView):
+    queryset = Post.published.all()
+    template_name = 'list.html'
+    context_object_name = 'posts'
+    paginate_by = 6
 
 
 def post_detail(reqest, pk):
